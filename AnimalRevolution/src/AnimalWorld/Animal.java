f6e39326c692e0d20895347bc262096eb64a2238
David Benoit <benoitd9@gmail.com>
2014-11-09 14:13:58 -0500

    Added Gender Instance variable to Animal

--- AnimalRevolution/src/AnimalWorld/Animal.java
+++ AnimalRevolution/src/AnimalWorld/Animal.java
@@ -1,13 +1,14 @@
 package AnimalWorld;
 
 /**
- * 
- * 
+ *
+ *
  * Animal is the parent class of all creatures: Mammal
- * 
- * @author  Thomas Ashborn, David Benoit, Kevin Patraw, Nathan Plante
+ *
+ * @author Thomas Ashborn, David Benoit, Kevin Patraw, Nathan Plante
  */
-public abstract class Animal implements Organism{
+public abstract class Animal implements Organism {
+
     protected EatingStrategy eatingStrat;
     protected MovementStrategy moveStrat;
     protected int size;  //the higher the number the bigger the animal
@@ -17,67 +18,64 @@ public abstract class Animal implements Organism{
     protected boolean canEatLarger; //0 can not eat larger creatures, 1 can eat larger
     protected String name;    //the name of this Animal
     protected String gender; //the gender of the Animal 
-    
-    public Animal(String aniName, int[] attributes){
+
+    public Animal(String aniName, int[] attributes) {
         position = new int[2];
         position[0] = attributes[0];
         position[1] = attributes[1];
-        
-        if(attributes[2] == 1)
+
+        if (attributes[2] == 1) {
             moveStrat = new Fight();
-        else if(attributes[2] == 2)
+        } else if (attributes[2] == 2) {
             moveStrat = new Flight();
-        else
+        } else {
             moveStrat = new RandomMovement();
-        
-        if(attributes[3] == 1){
+        }
+
+        if (attributes[3] == 1) {
             eatingStrat = new Carnivore();
             isCannibal = true;
-        }
-        else if(attributes[3] == 2){
+        } else if (attributes[3] == 2) {
             eatingStrat = new Herbivore();
             isCannibal = false;
-        }
-        else{
+        } else {
             eatingStrat = new Omnivore();
             isCannibal = false;
         }
-        
+
         size = attributes[4];
-        
+
         movementSpeed = attributes[5];
-        
+
     }
-    
-    int eat()  //returns 0 if it did not eat and 1 if it did eat
+
+    int eat() //returns 0 if it did not eat and 1 if it did eat
     {
-        return 0;   
+        return 0;
     }
-    
-    void move()
-    {
-        
+
+    void move() {
+
     }
-    
-    void takeTurn()
-    {
-        
+
+    void takeTurn() {
+
     }
-    
+
     @Override
-    public int[] getPosition(){
+    public int[] getPosition() {
         return position;
     }
-    
+
     @Override
-    public int getX(){
+    public int getX() {
         return position[0];
     }
-    
+
     @Override
-    public int getY(){
+    public int getY() {
         return position[1];
     }
-    
+
     abstract int hide(); //returns a 0 if unable to hide and a 1 if hide was successful
 }
