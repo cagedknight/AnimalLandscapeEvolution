c4a6224e37619a80fe1ce79b4c04f34988e2db91
David Benoit <benoitd9@gmail.com>
2014-11-09 14:43:09 -0500

    nate setup test

--- AnimalRevolution/src/AnimalWorld/Arm.java
+++ AnimalRevolution/src/AnimalWorld/Arm.java
@@ -1,4 +1,5 @@
 /*
+ * Nate can you see this?
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
